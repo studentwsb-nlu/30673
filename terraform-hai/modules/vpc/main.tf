7bccef1cf18c4b9888b8766d041d561c2efb32c3
studentwsb-nlu <pnowoszynski@student.wsb-nlu.edu.pl>
2025-06-01 17:44:35 +0200

    adding internet gateway

--- terraform-hai/modules/vpc/main.tf
+++ terraform-hai/modules/vpc/main.tf
@@ -24,6 +24,26 @@ resource "aws_subnet" "db" {
   tags = { Name = "db-subnet-${count.index}" }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "main-igw" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = { Name = "public-rt" }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 data "aws_availability_zones" "available" {}
 
 output "vpc_id" {
