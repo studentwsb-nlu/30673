0afcab444fb9ef54b09e6bb075289b67fa29906e
studentwsb-nlu <pnowoszynski@student.wsb-nlu.edu.pl>
2025-06-04 16:41:25 +0200

    updating user_data

--- terraform-hai/modules/ec2/user_data.sh
+++ terraform-hai/modules/ec2/user_data.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-# Install Node.js and start a simple app
 apt-get update
 apt-get install -y curl git
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
 
-# Copy app from /app directory (within the same repo)
-cp -r /app /home/ubuntu/app
+git clone https://github.com/studentwsb-nlu/30673app.git /home/ubuntu/app
 cd /home/ubuntu/app
 
-# Create .env file with sample data (in production, values might be passed through Terraform)
 cat <<EOF > .env
 DB_HOST=localhost
 DB_USER=example
